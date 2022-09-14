2a6e3088ab7328191820fb1335f6d29f8422fd78
markkex <avila.marco.dev@gmail.com>
2022-09-14 17:54:00 +0100

    add material-ui, cheerio. added scrapper and tabs on the pages

---
+++ src/components/Tabs.js
@@ -0,0 +1,66 @@
+import * as React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Scrapper from "../pages/Scrapper";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="Pesquisador (Em trabalho)" {...a11yProps(0)} />
+          <Tab label="Scrapper (Imovirtual)" {...a11yProps(1)} />
+          <Tab label="teste (ignorar)" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <Scrapper />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </Box>
+  );
+}
---
+++ src/pages/Scrapper.js
@@ -0,0 +1,129 @@
+import { useEffect, useState } from "react";
+import * as cheerio from "cheerio";
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+const Scrapper = () => {
+  const [textUrl, setTextUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+  const fetchingArray = [];
+  const fetchHousesScrapper = () => {
+    fetch(`https://cors-anywhere.herokuapp.com/${textUrl}`)
+      .then(function (response) {
+        // When the page is loaded convert it to text
+        return response.text();
+      })
+      .then(function (html) {
+        // Initialize the DOM parser
+        var parser = new DOMParser();
+        let names = [];
+        let link = [];
+        let tipology = [];
+        let area = [];
+        let pricePerMetter = [];
+        let offerPrice = [];
+        let location = [];
+        let images = [];
+        // Parse the text
+        var doc = parser.parseFromString(html, "text/html");
+        let $ = cheerio.load(html);
+        // You can now even select part of that html as you would in the regular DOM
+        // Example:
+        // var docArticle = doc.querySelector('article').innerHTML;
+        $(".offer-item-title").each(function (i, element) {
+          let name = $(this).prepend().text();
+          name = name.replace("\n", "");
+          name = name.trim();
+          names.push(name);
+        });
+
+        $("article .offer-item-details .offer-item-header h3 a").each(function (
+          i,
+          element
+        ) {
+          let linkhref = $(this).prepend().attr("href");
+          link.push(linkhref);
+        });
+
+        $(".offer-item-rooms").each(function (i, element) {
+          let tipologyType = $(this).prepend().text();
+          tipology.push(tipologyType);
+        });
+
+        $(".offer-item-area").each(function (i, element) {
+          let areaSize = $(this).prepend().text();
+          area.push(areaSize);
+        });
+        $(".offer-item-price-per-m").each(function (i, element) {
+          let perMeter = $(this).prepend().text();
+          pricePerMetter.push(perMeter);
+        });
+        $(".offer-item-price").each(function (i, element) {
+          console.log(element);
+          let price = $(this).prepend().text();
+          price = price.replace("\n", "");
+          price = price.trim();
+          offerPrice.push(price);
+        });
+        $(".offer-item-details .offer-item-header p").each(function (
+          i,
+          element
+        ) {
+          let locationArea = $(this).prepend().text();
+          locationArea = locationArea.replace("\n", "");
+          let filtered = locationArea.substring(25);
+          locationArea = filtered.trim();
+          location.push(locationArea);
+        });
+
+        $(".offer-item figure").each(function (i, element) {
+          let imagesParsing = $(this).prepend().attr("data-quick-gallery");
+          JSON.parse(imagesParsing);
+          console.log(imagesParsing);
+          imagesParsing = imagesParsing;
+
+          images.push(imagesParsing);
+        });
+
+        console.log(images);
+        fetchingArray.description = names;
+        fetchingArray.links = link;
+        fetchingArray.tipology = tipology;
+        fetchingArray.area = area;
+        fetchingArray.meterPrice = pricePerMetter;
+        fetchingArray.offerPrice = offerPrice;
+        fetchingArray.location = location;
+
+        console.log(fetchingArray);
+      })
+      .catch(function (err) {
+        console.log("Failed to fetch page: ", err);
+      });
+    setLoading(false);
+  };
+
+  return (
+    <div>
+      <h1>Scrapper (Imovirtual)</h1>
+      <div>
+        <TextField
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+          value={textUrl}
+          onChange={(e) => setTextUrl(e.target.value)}
+        />
+      </div>
+      <div style={{ paddingTop: "20px" }}>
+        <Button variant="contained" onClick={() => fetchHousesScrapper()}>
+          Contained
+        </Button>
+      </div>
+
+      {loading !== true && <div>cenas</div>}
+    </div>
+  );
+};
+
+export default Scrapper;
